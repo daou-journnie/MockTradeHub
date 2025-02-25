41a17f05273d17c1db718bdcee8b9cd9b5f53239
journnie <journnie@daou.co.kr>
2025-02-25 15:36:49 +0900

    mybatis setting

--- src/main/java/org/example/mocktradehub/util/MyBatisSessionFactory.java
+++ src/main/java/org/example/mocktradehub/util/MyBatisSessionFactory.java
@@ -5,15 +5,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.Reader;
-import java.util.logging.Logger;
 
-// SqlSession 객체를 뽑아내는 일 - SqlSession이 있어야 SQL 실행 가능
 public class MyBatisSessionFactory {
     private static SqlSessionFactory sqlSessionFactory;
 
     static {
-        Logger logger = Logger.getLogger(MyBatisSessionFactory.class.getName());
         try {
+            System.out.println("MyBatisSessionFactory");
+
             String resource = "./SqlMapConfig.xml";
             Reader reader = Resources.getResourceAsReader(resource);
             if(sqlSessionFactory == null) {
